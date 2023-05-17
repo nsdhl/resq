3e6e3cab4d3c6272bf970e7649ba107e3b6200e9
nsdhl <dhlnischal10@gmail.com>
2023-05-17 09:55:11 +0545

    type error for bcrypt

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
-import incidentRoutes from "./incident.route"
+import incidentRoutes from "./incident.route";
 
 const router = express.Router();
 
 router.use("/incident", incidentRoutes)
-
+router.use("/user/regiser", )
 export default router;
