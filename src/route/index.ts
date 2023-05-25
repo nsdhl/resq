bb4840beeed4cf17338756f1f9125aea165a0a5c
nsdhl <dhlnischal10@gmail.com>
2023-05-25 11:11:26 +0545

    added socket.io package

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,13 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
-import incidentRoutes from "./incident.route";
-import userRoute from "./user.route";
-const router = express.Router();
-
-router.use("/incident", incidentRoutes)
-router.use("/user", userRoute)
-
-=======
 
 import incidentRoutes from "./incident.route"
 import userRoutes from "./user.route";
@@ -15,5 +6,4 @@ const router = express.Router();
 
 router.use("/incident", incidentRoutes)
 router.use("/user", userRoutes);
->>>>>>> 2ef5485 (pushing staged files to user-auth)
 export default router;
