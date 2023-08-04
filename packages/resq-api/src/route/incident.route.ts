fba6580cebecef14104834071333a144f07ebe26
aatbip <anantabipal2010@gmail.com>
2023-08-04 22:28:32 +0545

    added incident marker on map

--- packages/resq-api/src/route/incident.route.ts
+++ packages/resq-api/src/route/incident.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createNewIncident, getByUser } from "../controllers/incident.controller";
+import { createNewIncident, getAllIncident, getByUser } from "../controllers/incident.controller";
 import { isLogin } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.post("/", isLogin, createNewIncident)
 router.get("/user", isLogin, getByUser)
+router.get("/", getAllIncident)
 
 export default router;
--- packages/resq-ui/src/components/Map.tsx
+++ packages/resq-ui/src/components/Map.tsx
@@ -1,16 +1,46 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
+import { url } from '../axios';
+import { GppMaybe } from '@mui/icons-material';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { divIcon } from 'leaflet';
+
+
+const iconMarkup = renderToStaticMarkup(
+  <GppMaybe />
+);
+const customMarkerIcon = divIcon({
+  html: iconMarkup
+});
 
 function MapComponent() {
+  const [incident, setIncident] = useState<any>([]);
+
+  useEffect(() => {
+    (
+      async () => {
+        const { data } = await url.get("/incident");
+        setIncident(data)
+      }
+    )()
+  }, [])
+
   return (
     //@ts-ignore
     <MapContainer center={[27.700769, 85.3214]} zoom={14} style={{ height: '100vh', width: '100%' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {
+        incident && incident.map((el: any, key: number) => {
+          return (
+            <Marker icon={customMarkerIcon} key={key} position={el?.location.coordinates}>
+              <Popup>
+                {el?.name}: {el?.description}
+              </Popup>
+            </Marker>
+          )
+        })
+      }
     </MapContainer>
   );
 }
