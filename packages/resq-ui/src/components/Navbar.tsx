94d6f4849c5329ac10c15d0fab3cda654f026a13
nsdhl <dhlnischal10@gmail.com>
2023-08-05 21:53:13 +0545

    updated mui of report and sigin page

--- packages/resq-ui/src/components/Navbar.tsx
+++ packages/resq-ui/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import { AppBar, Box, Button, IconButton, Stack, Toolbar } from '@mui/material'
-import { ErrorOutline, Menu } from "@mui/icons-material"
-import { useNavigate } from 'react-router-dom'
-import { registerServiceWorker, subscribe } from '../helpers/registerSw';
+import { AppBar, Box, Button, IconButton, Stack, Toolbar } from "@mui/material";
+import { ErrorOutline } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { registerServiceWorker, subscribe } from "../helpers/registerSw";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const user = JSON.parse(localStorage.getItem("user") as string)
+  const user = JSON.parse(localStorage.getItem("user") as string);
 
   const notification = async () => {
     try {
@@ -15,11 +15,16 @@ const Navbar = () => {
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#b5b7ba",
+        }}
+      >
         <Toolbar>
           <IconButton
             size="large"
@@ -28,29 +33,48 @@ const Navbar = () => {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            < Menu />
+            {/* <Menu /> */}
           </IconButton>
-          <Button onClick={notification} variant="contained" color="info" size="small" sx={{
-            fontSize: "12px",
-            color: "white"
-          }}>Turn On Notification</Button>
-          <Stack direction="row" justifyContent="right" sx={{
-            width: "100%"
-          }}>
-            <Button onClick={() => {
-              if (user) {
-                return navigate('/report')
-              }
-              navigate('/signin')
-            }
-            } variant="contained" endIcon={<ErrorOutline />} color="error" sx={{
-              borderRadius: "100px",
-            }}>Report Incident</Button>
+          <Button
+            onClick={notification}
+            variant="contained"
+            color="info"
+            size="small"
+            sx={{
+              fontSize: "12px",
+              color: "white",
+            }}
+          >
+            Turn On Notification
+          </Button>
+          <Stack
+            direction="row"
+            justifyContent="right"
+            sx={{
+              width: "100%",
+            }}
+          >
+            <Button
+              onClick={() => {
+                if (user) {
+                  return navigate("/report");
+                }
+                navigate("/signin");
+              }}
+              variant="contained"
+              endIcon={<ErrorOutline />}
+              color="error"
+              sx={{
+                borderRadius: "100px",
+              }}
+            >
+              Report Incident
+            </Button>
           </Stack>
         </Toolbar>
       </AppBar>
     </Box>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- packages/resq-ui/src/pages/ReportIncidentPage.tsx
+++ packages/resq-ui/src/pages/ReportIncidentPage.tsx
@@ -1,60 +1,96 @@
-import { Button, Stack, TextField, Typography } from "@mui/material"
-import { useState } from "react"
-import { url } from "../axios"
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { url } from "../axios";
 
 const ReportIncidentPage = () => {
-
   const [incident, setIncident] = useState({
     location: "",
     description: "",
-    incidentName: ""
-  })
+    incidentName: "",
+  });
 
   return (
-    <Stack direction="column" rowGap="1.5rem" sx={{
-      width: "80%",
-      margin: "0 auto",
-      p: "2rem 0rem"
-    }}>
-      <Typography variant="h2">Report Incident</Typography>
-      <TextField onChange={(e) => {
-        setIncident(prev => {
-          return {
-            ...prev,
-            location: e.target.value
-          }
-        })
-      }} id="location" label="Location" variant="standard" />
-      <TextField onChange={(e) => {
-        setIncident(prev => {
-          return {
-            ...prev,
-            description: e.target.value
-          }
-        })
-      }} id="description" label="Description" variant="standard" />
-      <TextField onChange={(e) => {
-        setIncident(prev => {
-          return {
-            ...prev,
-            incidentName: e.target.value
-          }
-        })
-      }} id="incidentName" label="Incident Name" variant="standard" />
+    <Stack
+      direction="column"
+      rowGap="1.5rem"
+      sx={{
+        width: "50%",
+        margin: "0 auto",
+        p: "2rem 0rem",
+        backgroundColor: "#f7f7f7",
+        borderRadius: "8px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        marginTop: "5rem",
+      }}
+    >
+      <img
+        src="..//assets//resqlogo.png"
+        alt="Report Incident"
+        style={{ display: "block", margin: "0 auto", maxWidth: "200px" }}
+      />
+      {/* <Typography variant="h2" >Report Incident</Typography> */}
+      <TextField
+        onChange={(e) => {
+          setIncident((prev) => {
+            return {
+              ...prev,
+              location: e.target.value,
+            };
+          });
+        }}
+        id="location"
+        label="Location"
+        variant="outlined"
+      />
+      <TextField
+        onChange={(e) => {
+          setIncident((prev) => {
+            return {
+              ...prev,
+              description: e.target.value,
+            };
+          });
+        }}
+        id="description"
+        label="Description"
+        variant="outlined"
+      />
+      <TextField
+        onChange={(e) => {
+          setIncident((prev) => {
+            return {
+              ...prev,
+              incidentName: e.target.value,
+            };
+          });
+        }}
+        id="incidentName"
+        label="Incident Name"
+        variant="outlined"
+      />
 
-      <Button sx={{
-        width: { xs: "100%", md: "50%" },
-        margin: "0 auto"
-      }} variant="outlined" onClick={async () => {
-        const { data } = await url.post("/incident", {
-          location: [Number(incident.location.split(",")[0]), Number(incident.location.split(",")[1])],
-          description: incident.description,
-          incidentName: incident.incidentName
-        })
-        console.log(data)
-      }}>Submit</Button>
+      <Button
+        sx={{
+          width: { xs: "100%", md: "20%" },
+          margin: "0 auto",
+        }}
+        variant="contained"
+        onClick={async () => {
+          const { data } = await url.post("/incident", {
+            location: [
+              Number(incident.location.split(",")[0]),
+              Number(incident.location.split(",")[1]),
+            ],
+            description: incident.description,
+            incidentName: incident.incidentName,
+          });
+          console.log(data);
+        }}
+      >
+        Submit
+      </Button>
     </Stack>
-  )
-}
+  );
+};
 
-export default ReportIncidentPage
+export default ReportIncidentPage;
