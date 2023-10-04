da6806c6b0c52658a7f28d82f265620f5c568405
nsdhl <dhlnischal10@gmail.com>
2023-10-04 22:50:04 +0545

    updated overview admin page

--- packages/resq-ui/src/pages/admin/OverviewPage.tsx
+++ packages/resq-ui/src/pages/admin/OverviewPage.tsx
@@ -1,67 +1,82 @@
+import React, { useEffect, useState } from "react";
 import BasicCard from "../../components/Card";
-import { PieChart, pieArcLabelClasses  } from '@mui/x-charts/PieChart';
-// import { BarChart } from '@mui/x-charts/BarChart';
-export default function OverView(){
-    const data = [
-        { value: 2, label: 'Incident' },
-        { value: 2, label: 'SOS' },
-        { value: 4, label: 'Total Disasters Reported' },
-      ];
-      
-      const size = {
-        width: 1200,
-        height: 500,
-      };
-      
-    return (
-        <>
-        <div style={{display:'flex', width:'100%', height:'100%', flexWrap:"wrap", gap:"60px", justifyContent:"center" }}>
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
+import axios from "axios";
+
+export default function OverView() {
+  const [incidentCount, setIncidentCount] = useState(0);
+  const [sosCount, setSosCount] = useState(0);
+  const [totalDisastersCount, setTotalDisastersCount] = useState(0);
+
+  const size = {
+    width: 1200,
+    height: 500,
+  };
+
+   useEffect(() => {
+    const apiUrl = "http://localhost:5000/api/overview";
+
+    axios.get(apiUrl).then((repos) => {
+      const allData = repos.data;
+      setIncidentCount(allData.incidentCount);
+      setSosCount(allData.sosCount);
+      setTotalDisastersCount(allData.totalDisastersCount);
+    })
+
+    .catch((error) => {
+      console.error("Axios Error:", error);
+    });
+
+  }
+  , []);
+
+  const data = [
+    { label: 'Total Incident Reported', value: incidentCount },
+    { label: 'Total SOS Requests', value: sosCount },
+    { label: 'Total Disasters Reported', value: totalDisastersCount },
+  ];
+  return (
+    <>
+      <div style={{ display: 'flex', width: '100%', height: '100%', flexWrap: "wrap", gap: "60px", justifyContent: "center" }}>
         <div style={{ border: '1px solid black' }}>
-  <BasicCard
-    description={'Total Count of Incident Reported'}
-    title={<span style={{ fontSize: '24px' }}>Total Incident Reported</span>}
-    number={2}
-  />
-</div>
-<div style={{ border: '1px solid black' }}>
-  <BasicCard
-    description={'Cumulative number of SOS Request Received'}
-    title={<span style={{ fontSize: '24px' }}>Total Incident Reported</span>}
-    number={2}
-  />
-</div>
-<div style={{ border: '1px solid black' }}>
-  <BasicCard
-    description={'Total Count of Disaster'}
-    title={<span style={{ fontSize: '24px' }}>Total Incident Reported</span>}
-    number={2}
-  />
-</div>
+          <BasicCard
+            description={'Total Count of Incident Reported'}
+            title={<span style={{ fontSize: '24px' }}>Total Incident Reported</span>}
+            number={incidentCount}
+          />
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <BasicCard
+            description={'Cumulative number of SOS Request Received'}
+            title={<span style={{ fontSize: '24px' }}>Total SOS Requests</span>}
+            number={sosCount}
+          />
+        </div>
+        <div style={{ border: '1px solid black' }}>
+          <BasicCard
+            description={'Total Count of Disaster'}
+            title={<span style={{ fontSize: '24px' }}>Total Disasters Reported</span>}
+            number={totalDisastersCount}
+          />
+        </div>
 
-           <PieChart
-      series={[
-        {
-          arcLabel: (item) => `${item.label} (${item.value})`,
-          arcLabelMinAngle: 45,
-          data,
-        },
-      ]}
-      sx={{
-        [`& .${pieArcLabelClasses.root}`]: {
-          fill: 'white',
-          fontWeight: 'bold',
-        },
-      }}
-      {...size}
-    />
-      {/* <BarChart
-      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-      width={500}
-      height={300}
-    /> */}
-
-            </div>
-        </>
-    )
+        <PieChart
+          series={[
+            {
+              arcLabel: (item) => `${item.label} (${item.value})`,
+              arcLabelMinAngle: 45,
+              data,
+            },
+          ]}
+          sx={{
+            [`& .${pieArcLabelClasses.root}`]: {
+              fill: 'white',
+              fontWeight: 'bold',
+            },
+          }}
+          {...size}
+        />
+      </div>
+    </>
+  )
 }
